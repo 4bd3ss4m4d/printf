c943c10a8986c76005dd804b95532aeb9063cd99
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-04-24 18:52:08 +0000

    [Fix]: Changing entirely _printf.c syntax

--- _printf.c
+++ _printf.c
@@ -11,39 +11,29 @@
  */
 int _printf(const char *format, ...)
 {
-	conv_spec conversions[] = {
-	    {'c', print_char}, {'s', print_str}, {'%', print_prc}, {'\0', NULL}};
+	conv_spec convarr[] = {
+	    {"%c", print_char}, {"%s", print_str}, {"%%", print_prc}, {"\0", NULL}};
 	va_list args;
 	unsigned int i, j, valid_specifier_flag, num_chars = 0;
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		valid_specifier_flag = 0;
-		if (format[i] == '%')
+		for (j = 0; convarr[j].func != NULL; j++)
 		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-			for (j = 0; conversions[j].specifier != '\0'; j++)
+			if (format[i] == convarr[j].spec[0] && format[i + 1] == convarr[j].spec[1])
 			{
-				if (format[i] == conversions[j].specifier)
-				{
-					num_chars += conversions[j].func(args);
-					valid_specifier_flag = 1;
-					break;
-				}
-			}
-			if (!valid_specifier_flag)
-			{
-				num_chars += _putchar('%');
-				num_chars += _putchar(format[i]);
+				num_chars += convarr[j].func(args);
+				valid_specifier_flag = 1;
+				i++;
+				break;
 			}
 		}
-		else
+		if (!valid_specifier_flag)
 			num_chars += _putchar(format[i]);
 	}
 	va_end(args);
