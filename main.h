9babc7d4df171cc0303a636ab79518505a8fb73b
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-04-24 15:14:55 -0700

    Adding description for struct conv_spec in main.h

--- main.h
+++ main.h
@@ -3,12 +3,25 @@
 
 #include <stdarg.h>
 
-/* Define a struct for the conversion specifiers */
-typedef struct conversion_specifier
+/**
+ * struct conversion_specifier - struct to hold conversion specifier
+ * and its corresponding function
+ * @spec: a string representing the conversion specifie
+ * @func: a pointer to the function that handles the conversion
+ *
+ * Description: This struct is used to hold a conversion specifier
+ * and a pointer to the function
+ */
+struct conversion_specifier
 {
-        char *spec;
-        int (*func)(va_list);
-} conv_spec;
+	char *spec;
+	int (*func)(va_list);
+};
+
+/**
+ * conv_spec - Typedef for conversion_specifier
+ */
+typedef struct conversion_specifier conv_spec;
 
 /* Function prototypes */
 int _putchar(char c);
