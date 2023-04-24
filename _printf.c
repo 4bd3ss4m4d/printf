a1fc3c60e3134d6d91a7b6b50e79722622411f41
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-04-24 17:08:35 +0000

    [Fix] Changing _printf.c syntax

--- _printf.c
+++ _printf.c
@@ -11,45 +11,43 @@
  */
 int _printf(const char *format, ...)
 {
-	conv_spec conversions[] = {
-	    {'c', print_char},
-	    {'s', print_str}};
+	conv_spec conversions[] = {{'c', print_char}, {'s', print_str}, {'\0', NULL}};
 	va_list args;
-	unsigned int i;
-	int num_chars = 0, valid_specifier_flag = 0;
-	size_t num_conversions = sizeof(conversions) / sizeof(conversions[0]);
+	unsigned int i, j;
+	int num_chars = 0, valid_specifier_flag;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-	while (*format)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (*format == '%')
+		valid_specifier_flag = 0;
+		if (format[i] == '%')
 		{
-			format++;
-			for (i = 0; i < num_conversions; i++)
+			i++;
+			if (format[i] == '\0')
+				return (-1);
+			for (j = 0; conversions[j].specifier != '\0'; j++)
 			{
-				if (*format == conversions[i].specifier)
+				if (format[i] == conversions[j].specifier)
 				{
-					num_chars += conversions[i].func(args);
+					num_chars += conversions[j].func(args);
 					valid_specifier_flag = 1;
 					break;
 				}
 			}
 			if (!valid_specifier_flag)
 			{
-				_putchar('%');
-				_putchar(*format);
-				num_chars += 2;
+				num_chars += _putchar('%');
+				num_chars += _putchar(format[i]);
 			}
 		}
 		else
 		{
-			_putchar(*format);
-			num_chars++;
+			num_chars += _putchar(format[i]);
 		}
-		format++;
 	}
+	va_end(args);
 	return (num_chars);
 }
