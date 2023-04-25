1327aa12b69b2820b4fb2d0be59bc1a843a37e9b
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-04-24 22:33:12 -0700

    Fixing print_pointer.c

--- print_pointer.c
+++ print_pointer.c
@@ -10,15 +10,27 @@
  */
 int print_pointer(va_list args)
 {
-	void *p = va_arg(args, void *);
-	unsigned long int address = (unsigned long int)p;
-	int len = 0;
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
 
-	len += _putchar('0');
-	len += _putchar('x');
-	len += print_HEX(address);
+	p = va_arg(args, void *);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
 
-	return (len);
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = print_HEX(a);
+	return (b + 2);
 }
 
 /**
