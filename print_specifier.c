3b272f27a53e9b87acd5a6c637b1ad4ba227fc13
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-04-24 19:47:11 -0700

    Adding print_specifier()

---
+++ print_specifier.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include "stdarg.h"
+
+/**
+ * print_specifier - selects and calls the corresponding conversion function
+ *	       according to the conversion specifier.
+ * @format: the conversion specifier character.
+ * @args: the va_list of arguments for the conversion.
+ *
+ * Return: the number of characters printed.
+ */
+int print_specifier(char format, va_list args)
+{
+	int i = 0, len = 0;
+	conv_spec conversions[] = {
+	    {"c", print_char},
+	    {"s", print_str},
+	    {NULL, NULL}};
+
+	while (conversions[i].spec)
+	{
+		if (*conversions[i].spec == format)
+			len = conversions[i].func(args);
+		i++;
+	}
+
+	return (len);
+}
