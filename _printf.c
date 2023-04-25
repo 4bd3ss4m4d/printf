718a0bf7a380a8cf14773bc271dbea3db9bc653b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-04-25 21:09:00 +0000

    Fixing printf

--- _printf.c
+++ _printf.c
@@ -1,9 +1,39 @@
 #include "main.h"
 
 /**
+ * specifier_selector - check code
+ * @format: character specifier
+ * @va: variadic list
+ * Return: string length
+ */
+int specifier_selector(const char *format, va_list va)
+{
+	printer printers[] = {
+	  {"c", print_char},
+	  {"i", print_integer},
+	  {"d", print_integer},
+	  {"R", print_rot13},
+	  {"r", print_reversed_string},
+	  {"b", print_to_binary},
+	  {"o", print_octal},
+	  {"x", print_hexadecimal},
+	  {"X", print_hexadecimal_capitalized},
+	  {"u", print_unsigned_integer},
+	  {"s", print_string},
+	  {"S", print_non_printable},
+	  {"p", print_pointer},
+	  {NULL, NULL}
+	};
+	int j;
+
+	j = 0;
+	while (printers[j].print != NULL && *format != *(printers[j].type))
+		j++;
+	return (printers[j].print != NULL ? printers[j].print(va) : -1);
+}
+/**
  * _printf - check code
  * @text: text to display
- *
  * Return: string length
  */
 int _printf(const char *text, ...)
@@ -20,14 +50,8 @@ int _printf(const char *text, ...)
 		else if (condition == 1)
 		{
 			r = specifier_selector(text + i, va);
-			if (r < 0)
-			{
-				len += write(1, &c, 1);
-				if (text[i] != c)
-					len += write(1, text + i, 1);
-			}
-			else
-				len += r;
+			len += r < 0 ? write(1, &c, 1)
+			  + (text[i] == c ? 0 : write(1, text + i, 1)) : r;
 			condition = 0;
 		}
 		else
