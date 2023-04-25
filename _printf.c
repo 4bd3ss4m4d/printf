2ed9d7318a27fb8efb95e3cd7bdf2a05dc562b23
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-04-24 19:41:16 -0700

    [Fix]: changing _printf() structure

--- _printf.c
+++ _printf.c
@@ -1,51 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdbool.h>
 
 /**
- * _printf - produces output according to a format
- * @format: character string containing zero or more directives
- * @...: variable number of arguments to be printed
+ * _printf - function my printf
+ * @format: string whit format to print
  *
- * Return: the number of characters printed
+ * Return: number of chars that print
  */
-int _printf(const char *const format, ...)
+int _printf(const char *format, ...)
 {
-	conv_spec conv_ar[] = {
-	    {"%s", print_str},
-	    {"%c", print_char},
-	    {"%%", print_prc},
-	    {NULL, NULL}};
-
+	int text_length;
 	va_list args;
-	int i = 0, j, len = 0;
-	bool valid_format_specifier = false;
 
-	va_start(args, format);
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		valid_format_specifier = false;
-		for (j = 0; conv_ar[j].spec != NULL; j++)
-		{
-			if (format[i] == conv_ar[j].spec[0] && format[i + 1] == conv_ar[j].spec[1])
-			{
-				len += conv_ar[j].func(args);
-				valid_format_specifier = true;
-				i++;
-				break;
-			}
-		}
-		if (!valid_format_specifier)
-		{
-			_putchar(format[i]);
-			len++;
-		}
-	}
+	va_start(args, format);
+	text_length = 0;
+
+	text_length = print_format(format, args);
+
 	va_end(args);
-	return (len);
+
+	return (text_length);
 }
